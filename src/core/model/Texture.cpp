68db7a198a5057f85eecdc4d1f5142d6c0984ebe
Pro1d <topAffaire_pg@hotmail.fr>
2017-03-04 18:04:10 +0100

    Texture model

---
+++ src/core/model/Texture.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Texture.h"
+
+using namespace std;
+
+
+Texture::Texture(vec4 data, positive size) :
+	data_(data),
+	size_(size),
+	log2size_(0)
+{
+	while(((positive) 1 << log2size_) < size_)
+		log2size_++;
+
+	positive correct_size = (positive) 1 << log2size_;
+	sizeMask_ = correct_size-1;
+
+	if(size_ != correct_size) {
+		size_ = correct_size;
+		cerr << "Error: texture size must be power of 2." << endl;
+	}
+}
+
--- src/core/model/Texture.h
+++ src/core/model/Texture.h
@@ -1,13 +1,25 @@
 #ifndef __TEXTURE_H__
 #define __TEXTURE_H__
 
+#include "core/math/type.h"
+
+
 class Texture {
 	public:
-		extern inline vec4 getValue(integer u, integer v); // data[((u & sizeMask) | ((v & sizeMask) << log2size)) << 2]
-		vec4 data;
-		//positive size;
-		positive log2size;
-		positive sizeMask;
+		Texture(vec4 data, positive size);
+
+		inline vec4 getValue(integer u, integer v) {
+			return data_ + ((u & sizeMask_) | ((v & sizeMask_) << log2size_)) * VEC4_SCALARS_COUNT;
+		}
+		inline vec4 getValue(real u, real v) {
+			return getValue((integer) (u * size_), (integer) (v * size_));
+		}
+
+	private:
+		vec4 data_;
+		positive size_;
+		positive log2size_;
+		positive sizeMask_;
 };
 
 #endif
